18f6d9458aaf34a333f2af7e9cf6e13e2ffff5ea
emmajiugo <emmajiugo@gmail.com>
2018-10-12 15:41:49 +0100

    create a vue sdk for rave

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,81 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Welcome to Rave VueJS SDK</h1>
+    <rave :email="email"
+          :amount="amount"
+          :reference="reference"
+          :rave-key="raveKey"
+          :callback="callback"
+          :close="close"
+          :currency="currency"
+          :country="country"
+          :customer_firstname="customer.firstname"
+          :customer_lastname="customer.lastname"
+          :custom_title="custom.title"
+          :custom_description="custom.description"
+          :custom_logo="custom.logo"
+          :redirect_url="redirectUrl"
+          :payment_plan="paymentPlan"
+          :subaccounts="subaccounts"
+          :payment_method="paymentMethod"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Rave from './components/rave-modal.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Rave
+  },
+  data(){
+    return{
+      //you can set all values you want to pass and initialize it in the template above
+      raveKey: "FLWPUBK-56e4a2c6c9a6b58364bfd07fc1993e2c-X", // replace with your key from your dashboard
+      email: "test@example.com", // email address of your client
+      amount: 1,  // replace with your desired amount
+      currency: "NGN", // supported currencies NGN, USD, EUR, GBP UGX, TZX, GHS, KES, ZAR
+      country: "NG", // pass country NG, GH, KE, ZA 
+      customer: {
+        firstname: "", // add customer firstname
+        lastname: "" // add customer lastname
+      },
+      custom: {
+        title: "", // add custom title
+        description: "", // add a description
+        logo: "" // add logo
+      },
+      redirectUrl: "", // add redirect url
+      paymentPlan: "", // add payments plan ID here
+      paymentMethod: "", // add 'card' or 'account' if you want a specific feature. Leave empty if you want all features
+      subaccounts: {
+        id: "RS_73954F005E68DADF3483197D5CF13E1E", // id of the subaccount; get from your dashboard
+        transaction_split_ratio:"", //
+        transaction_charge_type: "", //include this if the you want a flat fee eg: flat
+        transaction_charge: "" // include the flat fee amount you want eg: 100
+      } // add a subaccount for split payments
+    }
+  },
+  computed: {
+    reference(){
+      let text = "";
+      let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+      for( let i=0; i < 10; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+      return text;
+    }
+  },
+  methods: {
+    callback: function(response){
+      console.log(response)
+    },
+    close: function(){
+      console.log("Payment closed")
+    }
   }
 }
 </script>
---
+++ src/components/rave-modal.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="rave">
+    <h3>{{ msg }}</h3>
+    <button class="button" v-on:click="payWithRave">Pay With Modal</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'rave',
+    props: {
+      isProduction: {
+          type: Boolean,
+          required: false,
+          default: false //set to true if you are going live
+      },
+      email: {
+          type: String,
+          required: true
+      },
+      amount: {
+          type: Number,
+          required: true
+      },
+      raveKey: {
+          type: String,
+          required: true
+      },
+      reference: {
+          type: String,
+          required: true
+      },
+      callback: {
+          type: Function,
+          required: true,
+          default: (response) => {}
+      },
+      close: {
+          type: Function,
+          required: true,
+          default: () => {}
+      },
+      metadata: {
+          type: Object,
+          default: () => { return {} }
+      },
+      currency: {
+          type: String,
+          default: 'NGN'
+      },
+      country: {
+          type: String,
+          default: 'NG'
+      },
+      customer_firstname: {
+          type: String,
+          default: ''
+      },
+      customer_lastname: {
+          type: String,
+          default: ''
+      },
+      custom_title: {
+          type: String,
+          default: ''
+      },
+      custom_description: {
+          type: String,
+          default: ''
+      },
+      custom_logo: {
+          type: String,
+          default: ''
+      },
+      redirect_url: {
+          type: String,
+          default: ''
+      },
+      payment_plan: {
+          type: Number,
+          default: String
+      },
+      subaccounts: {
+          type: Object,
+          default: () => { return {} }
+      },
+      payment_method: {
+          type: String,
+          default: ''
+      },
+      hosted_payment: {
+          type: Number,
+          default: ''
+      }
+    },
+    data() {
+      return {
+        msg: 'Rave Modal',
+        payload: {
+
+        }
+      }
+    },
+    created() {
+        const script = document.createElement('script')
+        script.src = (!this.isProduction)
+            ? 'https://ravesandboxapi.flutterwave.com/flwv3-pug/getpaidx/api/flwpbf-inline.js'
+            : 'https://api.ravepay.co/flwv3-pug/getpaidx/api/flwpbf-inline.js'
+        document.getElementsByTagName('head')[0].appendChild(script)
+    },
+    methods: {
+      payWithRave() {
+        window.getpaidSetup({
+          customer_email: this.email,
+          amount: this.amount,
+          txref: this.reference,
+          PBFPubKey: this.raveKey,
+          onclose: () => this.close(),
+          callback: response => this.callback(response),
+          meta: this.metadata,
+          currency: this.currency,
+          country: this.country,
+          customer_firstname: this.customer_firstname,
+          customer_lastname: this.customer_lastname,
+          custom_title: this.custom_title,
+          custom_description: this.custom_description,
+          custom_logo: this.custom_logo,
+          redirect_url: this.redirect_url,
+          payment_plan: this.payment_plan,
+          subaccounts: this.subaccounts,
+          payment_method: this.payment_method,
+          hosted_payment: this.hosted_payment
+        })
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .button {
+    color: rgb(85, 19, 207);
+    width: 250px;
+    height: 50px;
+    font-weight: 800;
+  }
+</style>
